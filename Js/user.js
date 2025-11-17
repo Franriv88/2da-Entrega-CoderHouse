b0e39b6551c3172ac1f5866a61a73fce0dfa1359
Francisco Rivas <frivasv2388@gmail.com>
2025-11-17 01:04:05 -0300

    Css
    Dando estilos varios

--- Js/user.js
+++ Js/user.js
@@ -4,59 +4,53 @@
 const productos = [
     {
         id: 1,
-        nombre: "Remera PowerLifting",
-        genero: "M",
+        nombre: "Gorra Deluxe",
         precio: 18500.00,
         descripcion: "Diseñada para levantamientos pesados, con costuras reforzadas y tela antideslizante.",
-        imgen: "../Recursos/imagenes/remeraNegra.png"
+        imgen: "../Recursos/imagenes/gorra.png"
     },
     {
         id: 2,
-        nombre: "Remera Muscle",
-        genero: "F",
+        nombre: "Botines Autoajustables",
+
         precio: 16200.50,
         descripcion: "Corte sin mangas para máxima libertad de movimiento en tus brazos.",
-        imgen: "../Recursos/imagenes/remeraOliva.png"
+        imgen: "../Recursos/imagenes/botines.png"
     },
     {
         id: 3,
-        nombre: "Pack Remeras surtidas",
-        genero: "M",
+        nombre: "Condensador de Flujo",
         precio: 14000.00,
         descripcion: "Algodón premium ultra-suave para los entrenamientos más intensos.",
-        imgen: "../Recursos/imagenes/PackRemeras.png"
+        imgen: "../Recursos/imagenes/condensador.png"
     },
     {
         id: 4,
-        nombre: "Pack Calzas Deportivas Flex",
-        genero: "F",
+        nombre: "Hover Board",
         precio: 23000.75,
         descripcion: "Ajuste perfecto y tela transpirable a prueba de sentadillas (squat-proof).",
-        imgen: "../Recursos/imagenes/licra.png"
+        imgen: "../Recursos/imagenes/board.png"
     },
     {
         id: 5,
-        nombre: "Buzo Hoodie Legacy",
-        genero: "M",
+        nombre: "DeLorean",
         precio: 32500.00,
         descripcion: "Interior frizado, ideal para calentar en invierno o para un look urbano.",
-        imgen: "../Recursos/imagenes/remera4.png"
+        imgen: "../Recursos/imagenes/delorean.png"
     },
     {
         id: 6,
-        nombre: "Top Deportivo Active",
-        genero: "F",
+        nombre: "Mr Fusion",
         precio: 19999.99,
         descripcion: "Soporte de alto impacto para corridas, saltos y cardio.",
-        imgen: "../Recursos/imagenes/remera3.png"
+        imgen: "../Recursos/imagenes/mrFusion.png"
     },
     {
         id: 7,
-        nombre: "Short de Entrenamiento Hybrid",
-        genero: "M",
+        nombre: "Almanaque 1950&#8209;2000",
         precio: 21000.00,
         descripcion: "Tela ligera y elástica, con bolsillo interno. Perfecto para crossfit.",
-        imgen: "../Recursos/imagenes/remera2.png"
+        imgen: "../Recursos/imagenes/almanaque.png"
     }
 ];
 
@@ -74,11 +68,15 @@ productos.forEach((producto) => {
 
     //diapsitiva 19 de "Plantillas literales e InneHTML"
     cardItem.innerHTML = `
-        <h2>${producto.nombre}</h2>
+        <h2 class="font-face">${producto.nombre}</h2>
         <img src="${producto.imgen}" alt="${producto.nombre}">
-        <p>Género: ${producto.genero}</p>
-        <p>Precio: $${producto.precio}</p>
-        <p>Descripción: ${producto.descripcion}</p>
+        <div class="product-info">
+            <p class="font-face">Precio: $${producto.precio}</p>
+            <p>Descripción: ${producto.descripcion}</p>
+        <div class="card-buttons">
+            <button class="addButton">Agregar</button>
+            <button class="deleteButton">Eliminar</button>
+        </div>
     `;
 
     //métopdo appendChild: agrega un nodo al final de la lista de hijos de un nodo padre especificado.
